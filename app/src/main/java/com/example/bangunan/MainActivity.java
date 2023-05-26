7cc7c0872285b8a2aa22f988e946defedd5d018a
WaannAlter <134024741+WaannAlter@users.noreply.github.com>
2023-05-26 20:12:49 +0700

    ⠀⠀⠀⠀⠀⠀⠀⣠⢞⠅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡞⠁⠀⠀⠀⢰⠂⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⡄⠀⡆⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⣴⣥⠏⠀⠀⠀⠀⠀⡆⠀⠀⠀⠀⢰⡟⠀⠀⢀⠄⢰⡏⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⢣⠀⠀⠸⡀⠸⠀⠀⠀ ⠀⠀⠀⠀⠀⠈⠙⡟⠀⠀⢀⠂⠀⢸⠃⠀⠀⠀⢠⡿⠁⠀⡠⠋⢠⠻⠀⠀⠀⡄⡁⠀⠘⡄⠀⠀⢠⠀⡄⠑⡀⠘⡀⠀⠀⠷⠀⠇⠀⠀ ⠀⠀⠀⠀⠀⠀⣼⢡⠀⠄⠀⠀⢀⡟⠀⠀⢀⠞⡘⠃⢀⠎⠁⠀⠎⠀⠐⠀⠰⢁⠀⠀⠀⢳⠀⠀⠸⠣⠸⡀⠱⡀⠁⢀⠀⡇⠀⠘⠀⠀ ⠄⠀⠀⠀⠀⢰⣷⡟⠰⠀⠀⠀⠈⠀⠀⠀⡜⡸⠀⠀⡎⠀⠀⠀⢀⠇⠀⠀⡄⢸⠀⠀⠀⠀⠳⡀⠰⠀⠁⢣⠀⢳⠀⠈⠀⠁⠀⠀⠀⠀ ⣥⣤⣄⢀⠀⠀⢹⡇⠄⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⡼⠀⠀⠀⢰⡞⠀⠀⢠⠂⠀⠀⠀⠀⠀⠀⠈⠀⠃⠀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣤⠀⣼⠁⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀⠈⠁⠀⠀⢀⡟⠀⠀⢰⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⢰⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⠚⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠁⣠⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⡄⠘⡄⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣯⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠀⣼⠀⣴⢇⣞⣹⡀⠀⠀⡄⠀⠀⠀⠀⣷⡀⣷⠀⣷⡀⢸⡀⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⣰⡇⣰⣿⣼⣿⣾⣿⣿⣧⠀⢰⣷⡀⠀⠀⣇⣿⣿⣿⣧⣉⣧⣸⣧⣠⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⡇⢰⠀⠀⢀⡸⠀⡀⠀⢸⣿⣇⡈⠙⠿⠻⣿⠿⣿⣿⠀⣼⡽⢻⡀⠀⣿⣿⣿⣿⣟⢿⣛⣿⣿⣿⡄⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣣⣿⣠⢀⣿⡇⢸⣧⠀⣿⣿⢻⡗⢲⣦⠬⠤⠴⠟⢿⣷⡟⢿⣸⣷⣸⣷⡴⣿⣛⣿⣿⣿⠿⠿⣿⠃⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣼⡿⣧⣟⣿⣄⢻⠃⠈⠁⠀⠁⠀⢀⠀⠀⢸⠉⡇⢸⡏⠙⠃⢸⠀⠀⠀⠀⠀⠀⠀⠀⠉⡇⠀⠀⠀⠀⠀ ⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⢙⢛⣧⣿⣿⢿⣿⡇⠀⠀⠀⠀⠀⠚⡏⠀⠀⠀⠀⠀⠘⠃⠀⠀⠀⠀⠀⢀⣤⠀⠀⠀⠀⠀⡇⢀⡄⣀⣴⡄ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡙⢿⠀⠀⠀⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⢠⣇⣋⣾⣿⣿⣧ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠉⢣⠀⠐⠂⠀⠀⠀⠀⠀⠀⠀⢺⣆⠀⡠⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠉⢁⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⢀⡾⠃⣾⣼⣿⣿⣿⣿ ⠉⠉⠉⠛⠛⠛⠛⠛⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣾⣷⣄⠰⠆⠀⠀⠀⠀⠐⠒⠒⠒⠒⠒⠀⢴⣿⣻⡿⣁⣶⡟⣇⣸⣿⣿⣿⣿⣿⣿ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣈⣿⣿⣿⣿⣿⣿⠗⢄⡀⠀⠀⠀⠀⠠⠤⠤⠄⠀⠠⠟⢋⣵⡾⠛⣽⢱⣿⣿⣿⣿⣿⣿⣿⣿ ⣤⣶⣤⣤⣀⣀⣠⣤⣤⣀⣀⣀⣀⣘⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠉⠲⢄⡀⠀⠀⠀⠀⠀⢀⣀⣴⠟⠋⡆⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⠀⠈⠑⠦⢤⣠⣴⣞⣩⣤⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ahahahahahahaha
    
    ahahahahha
    
    aku tidak salah dunia ini yang salah

--- app/src/main/java/com/example/bangunan/MainActivity.java
+++ app/src/main/java/com/example/bangunan/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.bangunan;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    private List<CartItem> cartItems;
+    private int totalPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +31,12 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.home:
-                        // Handle Home item click
-                        // Add your desired functionality here
+                        Intent homeIntent = new Intent(MainActivity.this, ActivityAll.class);
+                        startActivity(homeIntent);
                         return true;
                     case R.id.all:
-                        Intent allitemsIntent = new Intent(MainActivity.this, ActivityAll.class);
-                        startActivity(allitemsIntent);
+                        Intent allItemsIntent = new Intent(MainActivity.this, ActivityAll.class);
+                        startActivity(allItemsIntent);
                         return true;
                     case R.id.cart:
                         Intent cartIntent = new Intent(MainActivity.this, activityCart.class);
@@ -66,6 +71,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        cartItems = new ArrayList<>();
+        totalPrice = 0;
     }
 
     // Click listener method for "Alat" category
@@ -77,6 +85,51 @@ public class MainActivity extends AppCompatActivity {
     // Click listener method for "Perlengkapan" category
     public void onPerlengkapanClicked(View view) {
         Intent intent = new Intent(MainActivity.this, listPerlengkapancat.class);
+        startActivity(intent);
+    }
+
+    // Click listener method for adding items to the cart
+    public void onAddToCartClicked(View view) {
+        String itemName = "";
+        int itemPrice = 0;
+
+        switch (view.getId()) {
+            case R.id.buttonBor:
+                itemName = "Bor";
+                itemPrice = 500000;
+                break;
+            case R.id.buttonPalu:
+                itemName = "Palu";
+                itemPrice = 20000;
+                break;
+            case R.id.buttonobeng:
+                itemName = "Obeng";
+                itemPrice = 50000;
+                break;
+            case R.id.buttontang:
+                itemName = "Tang";
+                itemPrice = 50000;
+                break;
+            case R.id.buttonmeteran:
+                itemName = "Meteran";
+                itemPrice = 10000;
+                break;
+        }
+
+        addToCart(itemName, itemPrice);
+        openCartActivity();
+    }
+
+    private void addToCart(String itemName, int itemPrice) {
+        CartItem item = new CartItem(itemName, itemPrice);
+        cartItems.add(item);
+        totalPrice += itemPrice;
+    }
+
+    private void openCartActivity() {
+        Intent intent = new Intent(this, activityCart.class);
+        intent.putExtra("cartItems", (ArrayList<CartItem>) cartItems);
+        intent.putExtra("totalPrice", totalPrice);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bangunan/activityCart.java
+++ app/src/main/java/com/example/bangunan/activityCart.java
@@ -1,10 +1,16 @@
 package com.example.bangunan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +20,7 @@ public class activityCart extends AppCompatActivity {
     private RecyclerView recyclerView;
     private CartAdapter cartAdapter;
     private List<CartItem> cartItems;
+    private int totalAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,46 @@ public class activityCart extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         cartAdapter = new CartAdapter(cartItems);
         recyclerView.setAdapter(cartAdapter);
+
+        // Calculate the total amount
+        totalAmount = calculateTotalAmount(cartItems);
+
+        // Display the total amount
+        TextView totalAmountTextView = findViewById(R.id.textView8);
+        totalAmountTextView.setText("Rp" + String.valueOf(totalAmount));
+
+        // Set up bottom navigation view
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation_bottom_cart);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        Intent homeIntent = new Intent(activityCart.this, ActivityAll.class);
+                        startActivity(homeIntent);
+                        return true;
+                    case R.id.all:
+                        Intent allItemsIntent = new Intent(activityCart.this, ActivityAll.class);
+                        startActivity(allItemsIntent);
+                        return true;
+                    case R.id.cart:
+                        // Already in the cart activity, do nothing
+                        return true;
+                    case R.id.account:
+                        Intent accountIntent = new Intent(activityCart.this, Login_Activity.class);
+                        startActivity(accountIntent);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private int calculateTotalAmount(List<CartItem> items) {
+        int total = 0;
+        for (CartItem item : items) {
+            total += item.getItemPrice();
+        }
+        return total;
     }
 }
-
